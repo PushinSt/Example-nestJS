bb2e154da9c867e70dddc59ec5c5a6f988dbec9d
dstarostin <pushin.st@gmail.com>
2022-10-28 17:55:06 +0700

    fix: add comments and edit readme

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core'
-import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core' // NestFactory предоставляет несколько статических методов, позволяющих создать экземпляр приложения
+import { ValidationPipe } from '@nestjs/common' // проверка для всех входящих клиентских данных (проверка параметров в post, get запросах и др)
 import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+async function bootstrap() { //инициализация приложения
+  const app = await NestFactory.create(AppModule)  
   app.useGlobalPipes(new ValidationPipe()) // проверка запросов (входных параметров) get post put и др
   await app.listen(3000)
 }
