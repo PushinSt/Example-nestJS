8a9738c0ecca4a8e0a19b0312421a2822828014e
dstarostin <pushin.st@gmail.com>
2022-10-31 13:13:14 +0700

    feat: add docker

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module'
 async function bootstrap() { //инициализация приложения
   const app = await NestFactory.create(AppModule)  
   app.useGlobalPipes(new ValidationPipe()) // проверка запросов (входных параметров) get post put и др
-  await app.listen(3000)
+  const port = process.env.PORT || 3000;
+  await app.listen(port)
 }
 bootstrap()
